fe2d6618c28381c8ad44b8f81e3c56bb541115c8
Renan Felipe Zavagli Correa <renanfzcz@gmail.com>
2025-05-21 22:06:06 -0300

    GET game by id

---
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{   //usa extends para criar consutas e tudo mais no banco de dados pelo JPA
+
+}
